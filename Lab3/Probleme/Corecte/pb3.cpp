6530c07830b7ec800461c099785c723f415bb5f7
Gabip19 <gabrielpaslaru01@gmail.com>
2023-11-24 22:56:06 +0200

    lab3

---
+++ Lab3/Probleme/Corecte/pb3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    int n;
+    int x;
+    
+    cin >> n;
+    while (n > 0)
+    {
+        cin >> x;
+        sum = sum + x;
+        n = n - 1;
+    }
+
+    cout << sum;
+}
---
+++ Lab3/Probleme/Gresite/pb2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b; // eroare - fiecare pe o alta linie in MLP
+
+    cin >> a;
+    cin >> b;
+
+    cout << a + b;
+
+    return 0; // eroare - return nedefinit
+}
